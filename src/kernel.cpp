fa228afd4d08773647ee46373b34efdbb962ae28
CryptoGir <josh.eaker@gmail.com>
2014-11-06 14:47:35 -0500

    Fixed checkpoints

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -18,7 +18,7 @@ typedef std::map<int, unsigned int> MapModifierCheckpoints;
 static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     boost::assign::map_list_of
         ( 0, 0x0e00670bu )
-        ( 0, 0x089062d5b )
+        ( 109625, 0x89062d5b )
     ;
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic (testNet)
